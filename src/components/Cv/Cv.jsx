9d1cdcdd9cc050525b4260537a184b5b250163ec
ValenAquino <vaquino@frba.utn.edu.ar>
2023-01-24 18:29:20 -0300

    Se refactoriza el componente Cv de funcional a clase

--- src/components/Cv/Cv.jsx
+++ src/components/Cv/Cv.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { Header } from "./Header";
 import { Content } from "./Content";
 import { Aside } from "./Aside";
@@ -18,4 +18,27 @@ export function Cv({ personalInfo, experiences, education, a4 }) {
       <Aside personalInfo={personalInfo} />
     </div>
   );
+}
+
+export default class CvPreview extends Component {
+  render() {
+    const { personalInfo } = this.props;
+    const { experiences } = this.props;
+    const { education } = this.props;
+    const { a4 } = this.props;
+    const cvStyle = `bg-slate-50 ${a4}`;
+    const cvGrid = "grid grid-cols-3 grid-rows-6";
+
+    return (
+      <div className={`${cvStyle} ${cvGrid}`}>
+        <Header personalInfo={personalInfo} />
+        <Content
+          personalInfo={personalInfo}
+          experiences={experiences}
+          education={education}
+        />
+        <Aside personalInfo={personalInfo} />
+      </div>
+    );
+  }
 }
